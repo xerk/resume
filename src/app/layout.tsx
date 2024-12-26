d48adbe2e80939715083791e6f869159535c1482
Ahmed Mamdouh <gm.xerk@gmail.com>
2024-12-26 03:46:12 +0200

    feat: Enable system theme in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -99,7 +99,11 @@ export default function RootLayout({
           fontSans.variable
         )}
       >
-        <ThemeProvider attribute="class" defaultTheme="light">
+        <ThemeProvider 
+          attribute="class" 
+          defaultTheme="system" 
+          enableSystem
+        >
           <TooltipProvider delayDuration={0}>
             {children}
             <Navbar />
